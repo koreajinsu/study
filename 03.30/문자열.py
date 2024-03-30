4c8d99d7367f71b2bab21c779d3896b492baf5d0
parkjinsu <koreasoo4382@naver.com>
2024-03-30 21:36:46 +0900

    개인공부

--- 03.30/문자열.py
+++ 03.30/문자열.py
@@ -14,3 +14,25 @@ print(sentence3)
 sentence4 ="""알리바바에서 공부중입니다."""
 print(sentence4)
 
+print("a" + "b")
+print("a","b")
+
+# 방법 1
+print("나는 %d살 입니다." %20)        #%d 정수
+print("나는 %s을 좋아해요." %"파이썬") #%s 문자열
+print("APPLE 은 %c로 시작해요" % "A") #%c 문자 
+
+# %s 로만 적으면 
+print("나는 %s살 입니다." %20)
+print("나는 %s 과 %s색을 좋아해요." %("파란", "빨간")) 
+
+
+#방법 2
+
+print("나는 {}살 입니다.".format(20))
+print("나는 {} 색과 {}색을 좋아해요.".format("파란", "빨간"))
+print("나는 {0} 색과 {1}색을 좋아해요.".format("파란", "빨간"))
+print("나는 {1} 색과 {0}색을 좋아해요.".format("파란", "빨간"))
+
+#방법 3
+print("나는 {age} 살이며, {color}색을 좋아해요.".format(age =20, color="빨간"))

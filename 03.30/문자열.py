60ccf32a04b8159d0277bf39f53a3e29924a8667
parkjinsu <koreasoo4382@naver.com>
2024-03-30 13:36:00 +0900

    개인공부

---
+++ 03.30/문자열.py
@@ -0,0 +1,16 @@
+sentence = '나는 박진수 입니다'
+print(sentence)
+
+sentence2 = "파이썬을 배우고 있습니다."
+print(sentence2)
+
+sentence3= """
+나는 박진수 입니다, 
+파이썬을 배우고 있습니다.
+"""
+#""" """을 사용하면 4줄이 찍힌다.
+print(sentence3)
+
+sentence4 ="""알리바바에서 공부중입니다."""
+print(sentence4)
+
---
+++ 03.30/연산, 숫자처리.py
@@ -0,0 +1,34 @@
+print(2 + 3 * 4) #14
+print((2 + 3) * 4) #20
+number = 2 + 3 * 4
+print(number) 
+
+number = number + 2 #16
+print(number)
+number += 2 #18
+print(number)
+number *=2 #36
+print(number)
+
+number /= 2 #18
+print(number)
+number -= 2 # 16
+print(number)
+print('ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ')
+
+number %= 2 #0 나머지
+print(number)
+
+print(abs(-5)) #5 abs절댓값 포현
+print(pow(4, 2)) #4^2 = 16  4의 2승
+print(max(5, 12)) #12 맥스값
+print(min(5, 12)) #5 최소값
+print(round(3.14)) # 3 반올림계산
+print(round(4.99)) # 5
+
+print('ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ')
+
+#맨 위에
+#from random import * 선언 후
+
+#print(randint(1, 45)) 1~45 까지 임의의 값 생성

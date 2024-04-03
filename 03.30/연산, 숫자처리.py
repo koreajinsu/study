bc940757d37072c37c3a6fa0d87f281d9d88d883
parkjinsu <koreasoo4382@naver.com>
2024-04-03 17:28:10 +0900

    문자열 연산 숫자처리

---
+++ 03.30/연산, 숫자처리.py
@@ -0,0 +1,38 @@
+print(2 + 3 * 4) #14
+print((2 + 3) * 4) #20
+number = 2 + 3 * 4 #14
+print(number) 
+
+number = number + 2 #16
+print(number)
+number += 2 #18
+print(number)
+number *=2 #36
+print(number)
+
+number /= 2 #18
+print(number)
+number -= 2 # 16
+print(number)
+print('ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ')
+
+number %= 2 #0 나머지
+print(number)
+
+print(abs(-5)) #5 abs절댓값 포현
+print(pow(4, 2)) #4^2 = 16  4의 2승
+print(max(5, 12)) #12 맥스값
+print(min(5, 12)) #5 최소값
+print(round(3.14)) # 3 반올림계산
+print(round(4.99)) # 5
+
+print('ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ')
+
+#맨 위에
+#from random import * 선언 후
+
+#print(randint(1, 45)) 1~45 까지 임의의 값 생성
+#갑자기 블루투스가 되지 않는다. 이걸 어떻게 해결을 해야되는지 모르겠다. 그리고 나는 저번에 
+#과거에 임예진의 학교 선배가 지금 내옆에 있. 그래서조금은 떨린다...
+#오는날이 장날 이었다 이벙네 저금은 떨리지만 그래도 나는공부를 하러온 학생이기 떄무네
+#끝까지 집중하는 척을 하면서 우선 집중을 하겠다..
